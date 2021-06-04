135ff8c0f08e0ff8485ae44e447f642c4c482573
krosty47 <c_coriasco@hotmail.com>
2021-06-04 16:01:17 -0300

    Update

---
+++ api/src/models/Breed.js
@@ -0,0 +1,27 @@
+const { DataTypes } = require('sequelize');
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+module.exports = (sequelize) => {
+    // defino el modelo
+    sequelize.define('breed', {
+        ID: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        weight: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        height: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        life_span: {
+            type: DataTypes.INTEGER,
+        },
+    });
+};
--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,12 +1,16 @@
 const { Router } = require('express');
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
-
+const dogRouter = require('./dogRouter');
+const dogsRouter = require('./dogsRouter');
+const temperamentRouter = require('./temperamentRouter');
 
 const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
-
+router.use('/dog', dogRouter);
+router.use('/dogs', dogsRouter);
+router.use('/temperament', temperamentRouter);
 
 module.exports = router;
